a9ecb8557ade2d9ad0a074e32fe1f9106353b540
Leonardo-Ozima <leonardoozima@alunos.utfpr.edu.br>
2024-10-07 00:20:03 -0300

    added typewriter, image modules

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import geiselImage from "./assets/images/geisel.png";
 
 export function Home() {
+  const [text1] = useTypewriter({
+    words: ["Ipsum", "Dolor", "Aeron"],
+    loop: 0,
+    typeSpeed: 100,
+    deleteSpeed: 30,
+    delaySpeed: 1500,
+  });
   return (
     <>
       <nav className="bg-black text-white fixed top-0 left-0 w-full z-50 shadow-lg">
@@ -22,13 +31,16 @@ export function Home() {
       <div className="flex bg-black w-full h-screen items-center justify-center">
         <div className="flex flex-col items-center justify-center space-y-4">
           <h1 className="font-bold text-glow shadow-lg text-white text-[200px] ">
-            LOREM IPSUM
+            Loren {text1}
+            <Cursor cursorStyle="|"></Cursor>
           </h1>
-          <h1 className="font-bold absolute transform translate-y-4  text-white text-[200px] text-outline text-texture">
-            LOREM IPSUM
+          <h1 className="font-bold absolute transform translate-y-1  text-white text-[200px] text-outline text-texture">
+            Loren {text1}
+            <Cursor cursorStyle="|"></Cursor>
           </h1>
           <h1 className="font-bold absolute transform -translate-y-5 text-white text-[200px] text-outline">
-            LOREM IPSUM
+            Loren {text1}
+            <Cursor cursorStyle="|"></Cursor>
           </h1>
         </div>
       </div>
@@ -39,7 +51,25 @@ export function Home() {
           <div className="h-2 w-11/12 bg-white m-3 shadow-[0_0_15px_5px_rgba(255,255,255,0.6)]"></div>
           <div className="h-2 w-10/12 bg-white m-1 "></div>
 
-          <div className=""></div>
+          <div className="flex flex-row h-3/4 w-4/5 bg-red-300  mt-16">
+            <div className="h-full p-14 flex items-center justify-center text-justify w-1/2">
+              <p className="sm:text-base md:text-xl lg:text-3xl">
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Arcu quam
+                inceptos inceptos orci ridiculus eget tristique cras. Ipsum
+                nascetur tempus, fusce varius torquent laoreet. Maecenas pretium
+                lacus nullam integer justo dictumst mauris. Nullam ac vulputate
+                morbi sit platea non quam amet ullamcorper.
+              </p>
+            </div>
+            <div className="border-4 border-white h-full w-2"></div>
+            <div className="flex h-full w-1/2 ">
+              <img
+                src={geiselImage}
+                alt="Building"
+                className="object-contain"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>
